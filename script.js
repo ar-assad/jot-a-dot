bcb4846b96c1512e9b06e8ce8258d34b29b9a63f
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-10-01 17:03:00 +0500

    Add pixel density changing functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+const canvas = document.querySelector('.canvas');
+
 function renderCanvas(canvasSize = 400, pixelSize = 16) {
     const root = document.querySelector(':root');
     root.style.setProperty('--canvas-size', canvasSize + 'px');
-    const canvas = document.querySelector('.canvas');
     const truePixelSize = canvasSize / pixelSize;
     for (let i = 0; i < pixelSize ** 2; i++) {
         const pixel = document.createElement('div');
@@ -14,3 +15,13 @@ function renderCanvas(canvasSize = 400, pixelSize = 16) {
 }
 
 renderCanvas();
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', ()=>{
+    let newDensity = +prompt("Enter new density __ × __ : ");
+    if (!Number.isInteger(newDensity) || newDensity == 0) return;
+    canvas.innerHTML = "";
+    if (newDensity > 128)
+        newDensity = 128;
+    renderCanvas(400, newDensity);
+})
