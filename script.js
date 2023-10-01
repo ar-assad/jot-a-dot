fc4e2cc8345820546e2c0febd945d28efde38277
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-10-01 18:42:37 +0500

    Update renderCanvas function

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function renderCanvas(canvasSize = 400, pixelSize = 16) {
     const root = document.querySelector(':root');
     root.style.setProperty('--canvas-size', canvasSize + 'px');
     const truePixelSize = canvasSize / pixelSize;
+    canvas.innerHTML = "";
     for (let i = 0; i < pixelSize ** 2; i++) {
         const pixel = document.createElement('div');
         pixel.style.height = pixel.style.width = truePixelSize + 'px';
@@ -20,7 +21,6 @@ const btn = document.querySelector('button');
 btn.addEventListener('click', ()=>{
     let newDensity = +prompt("Enter new density __ × __ : ");
     if (!Number.isInteger(newDensity) || newDensity == 0) return;
-    canvas.innerHTML = "";
     if (newDensity > 128)
         newDensity = 128;
     renderCanvas(400, newDensity);
